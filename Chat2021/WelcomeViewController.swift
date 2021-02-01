fe7a2bbaa52aac7da18644a205502b9398500cb8
Konstantin Dmitrievskiy <>
2021-02-01 21:41:57 +0600

    Initiall commit

---
+++ Chat2021/WelcomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chat2021
+//
+//  Created by Konstantin Dmitrievskiy on 01.02.2021.
+//  Copyright © 2021 Konstantin Dmitrievskiy. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
