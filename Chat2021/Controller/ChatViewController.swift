b476bc3dbe952931b9945c43fe30889f21e21afa
Konstantin Dmitrievskiy <>
2021-02-03 22:48:45 +0600

    Add three VC

---
+++ Chat2021/Controller/ChatViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChatViewController.swift
+//  Chat2021
+//
+//  Created by Konstantin Dmitrievskiy on 03.02.2021.
+//  Copyright © 2021 Konstantin Dmitrievskiy. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
